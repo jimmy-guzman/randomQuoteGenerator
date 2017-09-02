ddd03967ac58a205eed313e454d48078c3cb597e
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-09-02 16:51:18 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  getQuote();
+  $("#get-quote").on("click",function(){
+    getQuote();
+  });
+});
+
+
+function getQuote(){
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/?",
+    cache: false,
+    dataType: "jsonp",
+    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+    success: function(data){
+      $("#quote").text('"'+data.quoteText+'"');
+      $("#author").text("-"+data.quoteAuthor);
+    }
+  });
+}
