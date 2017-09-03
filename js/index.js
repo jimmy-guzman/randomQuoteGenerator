7d26dfe5381be8dbe91577480fe5dbdf17aa2ddf
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-09-02 23:58:25 -0500

    added tweet function

--- js/index.js
+++ js/index.js
@@ -14,7 +14,13 @@ function getQuote() {
         data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
         success: function (data) {
             $("#quote").text(data.quoteText);
-            $("#author").text(data.quoteAuthor + ' \u2014');
+            $("#author").text(data.quoteAuthor + "\u2014");
+            $('#tweetQuote').click(function () {
+                var currentQuote = encodeURIComponent('"' + data.quoteText + '"' + ' ' + '-' + data.quoteAuthor);
+                var tweetQuote = "https://twitter.com/intent/tweet?text=" + currentQuote;
+                window.open(tweetQuote, '_blank');
+            });
+
         }
     });
 }
