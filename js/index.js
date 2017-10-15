cf6cfb204712331a285d4881be2f51b635e8e749
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-10-14 23:14:34 -0500

    cleaned code and vertically align update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  getQuote();
+  $("#get-quote").on("click", function() {
     getQuote();
-    $("#get-quote").on("click", function () {
-        getQuote();
-    });
+  });
 });
 
 
 function getQuote() {
-    $.ajax({
-        url: "https://api.forismatic.com/api/1.0/?",
-        cache: false,
-        dataType: "jsonp",
-        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-        success: function (data) {
-            $("#quote").text(data.quoteText);
-            $("#author").text(data.quoteAuthor + "\u2014");
-            $('#tweetQuote').click(function () {
-                var currentQuote = encodeURIComponent('"' + data.quoteText + '"' + ' ' + '-' + data.quoteAuthor);
-                var tweetQuote = "https://twitter.com/intent/tweet?text=" + currentQuote;
-                window.open(tweetQuote, '_blank');
-            });
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/?",
+    cache: false,
+    dataType: "jsonp",
+    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+    success: function(data) {
+      $("#quote").text(data.quoteText);
+      $("#author").text(data.quoteAuthor + "\u2014");
+      $('#tweetQuote').click(function() {
+        var currentQuote = encodeURIComponent('"' + data.quoteText + '"' + ' ' + '-' + data.quoteAuthor);
+        var tweetQuote = "https://twitter.com/intent/tweet?text=" + currentQuote;
+        window.open(tweetQuote, '_blank');
+      });
 
-        }
-    });
+    }
+  });
 }
